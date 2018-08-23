140020113ff487805aa8ce5d4636b1c579344b20
drew-anthony <danth720@gmail.com>
2018-08-23 15:08:19 -0500

    initial commit

---
+++ public/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { ShelterComponent } from './shelter/shelter.component';
+import { DetailsComponent } from './details/details.component';
+import { NewReviewComponent } from './new-review/new-review.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+import { PageNotFoundComponent  } from './page-not-found/page-not-found.component';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ShelterComponent },
+  { path: 'new', component: CreateComponent },
+  { path: ':id', component: DetailsComponent},
+  { path: 'newreview/:id', component: NewReviewComponent },
+  
+  { path: ':id/edit', component: UpdateComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/pets' },
+  { path: '**', component: PageNotFoundComponent  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
